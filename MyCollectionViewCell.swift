8c722f019020022b189c1cfdd4c9e10697a8e095
golmic <ljq0225@gmail.com>
2017-02-25 20:04:37 +0800

    UILabel nil

---
+++ MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  hello
+//
+//  Created by 陆建强 on 2017/2/25.
+//  Copyright © 2017年 陆建强. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+}
